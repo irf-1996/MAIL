6abce50cf0997c9bfede874ae2caff73315397b5
irf-1996 <87318867+irf-1996@users.noreply.github.com>
2023-10-28 13:43:02 +0100

    first commit

---
+++ mail.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+// Create a transporter with your email service provider's SMTP settings
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'amaluirfana96@gmail.com',
+      pass: 'wplq ldbh crmz oacm'
+    }
+  });
+  
+
+// Email data
+const mailOptions = {
+  from: 'amaluirfana96@gmail.com',
+  to: 'amaluirfana96@gmail.com',
+  subject: 'Hello from Nodemailer',
+  text: 'This is a test email sent from Nodemailer!'
+};
+
+// Send the email
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.log('Error sending email: ' + error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
